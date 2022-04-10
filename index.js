883d26264e28d04d5d7e668dd986b6432a9abff9
jwho85 <98196552+jwho85@users.noreply.github.com>
2022-04-10 16:16:08 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ app.put('/users/:Username',
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
-
+  
   const port = process.env.PORT || 8080;
   app.listen(port, '0.0.0.0',() => {
     console.log('Listening on Port ' + port);
